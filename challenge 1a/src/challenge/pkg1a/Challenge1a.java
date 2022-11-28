39c215c034c74f7f577d8080c130205bcbf61fd6
Shevia Neyliana <89763537+shevianeyll@users.noreply.github.com>
2022-11-28 19:35:17 +0700

    Add files via upload

---
+++ challenge 1a/src/challenge/pkg1a/Challenge1a.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package challenge.pkg1a; // java package
+
+/**
+ *
+ * @author ASUS
+ */
+public class Challenge1a { // java class
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // main program
+        String nomortransaksi, totalharga, totalpembayaran;
+     int kembalian;
+     String tanggaldanwaktupembayaran;
+        // TODO code application logic here
+        nomortransaksi="365";
+        totalharga="50.000";
+        totalpembayaran="100.000";
+                        kembalian=50000;
+                        tanggaldanwaktupembayaran="08/09/2021";
+        
+    System.out.println("nomortransaksi: "+ nomortransaksi);
+    System.out.println("totalharga: "+ totalharga);
+    System.out.println("totalpembayaran: "+ totalpembayaran);
+    System.out.println("kembalian: "+ kembalian);
+    System.out.println("tanggaldanwaktupembayaran: "+ tanggaldanwaktupembayaran);
+    }
+    
+}
