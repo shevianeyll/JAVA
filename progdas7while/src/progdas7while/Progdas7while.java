41ed44c46ffc3308851fb080da44e35a24f3cf93
Shevia Neyliana <89763537+shevianeyll@users.noreply.github.com>
2022-11-29 07:43:37 +0700

    Add files via upload

---
+++ progdas7while/src/progdas7while/Progdas7while.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progdas7while;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Progdas7while {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+ int a = 5;
+    int b = 3;
+    int total = 0;
+    for(int i=1; i<=3; i++){
+        for(int j=1; j<=4; j++){
+            System.out.print(a + "\t");
+            a = a + b; 
+            
+        }
+        System.out.println();
+}
+    }
+}
---
+++ projekpertamaku/src/projekpertamaku/Projekpertamaku.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekpertamaku; // java package
+
+/**
+ *
+ * @author ASUS
+ */
+public class Projekpertamaku { // java class
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // main program
+        // TODO code application logic here
+        System.out.println("nama: shevia neyliana");
+        System.out.println("kelas: XR7");
+        System.out.println("nomor: 32");
+        System.out.println("alamat: tuban jawa timur");
+        System.out.println("no hp: 085784074691");
+        System.out.println("sekolah: smk telkom malang");
+        System.out.println("hobi: nonton drakor");
+    } 
+    
+}
